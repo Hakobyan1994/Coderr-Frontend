c7441bbf30e08505e8692a878b2302064e50efda
Hakobyan1994 <mesrop.hakobyan@web.de>
2025-08-29 23:47:15 +0200

    added up but

--- shared/scripts/config.js
+++ shared/scripts/config.js
@@ -9,8 +9,8 @@ const GUEST_LOGINS = {
     }
 }
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api/';
-const STATIC_BASE_URL = 'http://127.0.0.1:8000/';
+const API_BASE_URL = 'https://api.coderr-app.com/api/';
+const STATIC_BASE_URL = 'https://api.coderr-app.com/';
 
 
 const LOGIN_URL = 'login/';
